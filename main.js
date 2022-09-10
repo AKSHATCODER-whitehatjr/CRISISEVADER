8cee9b7f49f87912131873cda710f8fb3f93d177
AKSHATCODER-whitehatjr <79800272+AKSHATCODER-whitehatjr@users.noreply.github.com>
2022-09-09 21:00:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function ukr(){
+window.location = "ukr.html";    
+}
+
+function CC(){
+    window.location = "CC.html";    
+    }
+
+function D(){
+   window.location = "D.html"; 
+}
+
+function P(){
+window.location = "P.html"; 
+}
